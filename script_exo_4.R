4469b8647ba4f62744a8b10639c2a04d3cdfcb40
Isaline <isalineherve@gmail.com>
2025-02-05 14:12:25 +0100

    Question 6

--- script_exo_4.R
+++ script_exo_4.R
@@ -36,3 +36,40 @@ indice_gini <- df_DEC_COM_Meta |>
 print(indice_gini)
 
 # Question 4
+
+library(dplyr)
+library(stringr)
+
+var_d9_d1 <- df_DEC_COM_Meta |>
+  filter(str_detect(LIB_VAR_LONG, regex("Rapport interdécile D9/D1", ignore_case = TRUE)))
+
+print(var_d9_d1)
+
+cod_d9_d1 <- var_d9_d1$COD_VAR[1]
+
+d9_d1_par_dept <- df_DEC_COM |>
+  select(COD_VAR, !!sym(cod_d9_d1)) |>
+  rename(Rapport_D9_D1 = !!sym(cod_d9_d1)) |>
+  group_by(COD_VAR) |>
+  summarise(Rapport_D9_D1 = mean(Rapport_D9_D1, na.rm = TRUE), .groups = "drop")
+
+dept_min <- d9_d1_par_dept |> slice(which.min(Rapport_D9_D1))
+dept_max <- d9_d1_par_dept |> slice(which.max(Rapport_D9_D1))
+
+print(paste("Département avec le plus faible D9/D1 :", dept_min$COD_VAR, "(", dept_min$Rapport_D9_D1, ")"))
+print(paste("Département avec le plus élevé D9/D1 :", dept_max$COD_DEP, "(", dept_max$Rapport_D9_D1, ")"))
+
+#????
+
+# Question 5
+head(df_DEC_COM)
+# Ce dataframe semble contenir des caractéristiques socio-économiques (comme l'age par ex) des personnes par code géographique.
+# "s" veut peut-être dire que les informations manquent
+
+# Question 6
+library(dplyr)
+
+DEC_COM_2 <- df_DEC_COM |>
+  filter(CODGEO %in% c("44109", "97416"))|>
+  mutate_all(~ gsub(",", ".", ., fixed = TRUE))|>
+  mutate_all(as.numeric)
